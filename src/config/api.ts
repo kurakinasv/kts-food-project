ede19a5daaf9826b56329fd4d064ff3ae3fb93a8
kurakinasv <svetakurakina2002@gmail.com>
2023-03-22 20:18:33 +0300

    removed base url from envs

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = import.meta.env.BASE_URL;
-
 export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const BASE_URL = 'https://api.spoonacular.com/recipes';
 
 export const ingredientsBaseUrl = 'https://spoonacular.com/cdn/ingredients_100x100/';
