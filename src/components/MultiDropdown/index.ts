b3c9348bea33ddb6274df21b6610e521de79a112
kurakinasv <svetakurakina2002@gmail.com>
2023-03-22 19:17:16 +0300

    added env

--- src/components/MultiDropdown/index.ts
+++ src/components/MultiDropdown/index.ts
@@ -1,2 +1,1 @@
 export { default } from './MultiDropdown';
-export type { Option } from './MultiDropdown';
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://api.spoonacular.com/recipes';
+export const BASE_URL = import.meta.env.BASE_URL;
 
-export const API_KEY = 'f0eaf4ba44534bb0ba44b29b787ad537';
+export const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const ingredientsBaseUrl = 'https://spoonacular.com/cdn/ingredients_100x100/';
